35c19ea165e09b1ce3fe41d2b51017ae2aae16f8
Domantas Jovaisas <domantas.jovaisas@presentconnection.nl>
2015-03-13 10:12:56 +0200

    Nuget package instructions

--- Isidos.Paysera/Properties/AssemblyInfo.cs
+++ Isidos.Paysera/Properties/AssemblyInfo.cs
@@ -1,17 +1,16 @@
 ﻿using System.Reflection;
-using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("Isidos.Paysera")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription(".NET library for payments through Paysera.com gateway")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Isidos")]
 [assembly: AssemblyProduct("Isidos.Paysera")]
 [assembly: AssemblyCopyright("Copyright ©  2015")]
-[assembly: AssemblyTrademark("")]
+[assembly: AssemblyTrademark("Isidos.Paysera")]
 [assembly: AssemblyCulture("")]
 
 // Setting ComVisible to false makes the types in this assembly not visible 
@@ -32,5 +31,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.2.0.0")]
+[assembly: AssemblyFileVersion("1.2.0.0")]
